875181c0afe96c773243e81146a6bbdf20ac3223
IkrarAB <ikraranugrah26@gmail.com>
2019-09-23 23:00:53 +0700

    Initial commit

---
+++ src/pbo1/pkg10118033/latihan6/PBO110118033Latihan6Mamalia.java
@@ -0,0 +1,14 @@
+package pbo1.pkg10118033.latihan6;
+
+/*
+ *
+ * @author
+ * NAMA  : Ikrar Anugrah Bastary
+ * KELAS : PBO1
+ * NIM   : 10118033
+ *
+ */
+
+public class PBO110118033Latihan6Mamalia {
+    public static int jumlahKambing;
+}
---
+++ src/pbo1/pkg10118033/latihan6/PBO110118033Latihan6Tester.java
@@ -0,0 +1,19 @@
+package pbo1.pkg10118033.latihan6;
+
+/*
+ *
+ * @author
+ * NAMA  : Ikrar Anugrah Bastary
+ * KELAS : PBO1
+ * NIM   : 10118033
+ *
+ */
+
+public class PBO110118033Latihan6Tester {
+    public static final String NAMA_KAMBING = "MIDUN";
+    
+    public static void main(String[] args) {
+        PBO110118033Latihan6Mamalia.jumlahKambing = 10000;
+        System.out.println(NAMA_KAMBING + " memiliki kambing sebanyak " + PBO110118033Latihan6Mamalia.jumlahKambing);
+    }
+}
